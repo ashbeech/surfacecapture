92d43eaadae1d7b31a97ff1317d9a78c2edeecee
Ash Beech <ash@bullish.design>
2025-01-03 13:59:05 +0000

    Initial Commit

---
+++ Surface CaptureTests/Surface_CaptureTests.swift
@@ -0,0 +1,17 @@
+//
+//  Surface_CaptureTests.swift
+//  Surface CaptureTests
+//
+//  Created by Ashley Davison on 03/01/2025.
+//
+
+import Testing
+@testable import Surface_Capture
+
+struct Surface_CaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
